bd9db3c5b2f7817859957dc8b16d5d17ea724035
LucasLaurensHeyteam <lucas.laurens@heyteam.com>
2025-07-14 19:23:42 +0200

    refactor: add vite as async

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
+import fs from 'fs';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    process.env = {...process.env, ...env };
+
+    return defineConfig({
+        server: {
+            host: `app.${process.env.APP_SERVICE}`,
+            port: 5173,
+            https: {
+                key: fs.readFileSync(`./certs/${process.env.APP_SERVICE}.key`),
+                cert: fs.readFileSync(`./certs/${process.env.APP_SERVICE}.cert`),
+            },
+            cors: true,
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            tailwindcss(),
+        ],
+    });
+};
