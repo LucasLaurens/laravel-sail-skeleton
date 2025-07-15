a885abe506316bc1d29f465a93f3a8109b9a463f
LucasLaurensHeyteam <lucas.laurens@heyteam.com>
2025-07-15 09:27:11 +0200

    refactor: using vite with tls back server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\Models\User;
+use App\Models\Project;
+use App\Policies\UserPolicy;
+use App\Policies\ProjectPolicy;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,14 +22,54 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+    
     }
-
+    
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->configureVite();
+        $this->registerPolicies();
+    }
+    
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+    
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+    }
+    
+    private function configureUrl(): void
+    {
+        URL::forceScheme('https');
+    }
+    
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('aggressive');
+        Vite::prefetch(concurrency: 3);
+    }
+    
+    private function registerPolicies(): void
+    {
+        Gate::policy(
+            Project::class,
+            ProjectPolicy::class
+        );
+        
+        Gate::policy(
+            User::class,
+            UserPolicy::class
+        );
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default ({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
@@ -9,17 +10,24 @@ export default ({ mode }) => {
 
     return defineConfig({
         server: {
-            host: `app.${process.env.APP_SERVICE}`,
+            host: '0.0.0.0',
             port: 5173,
             https: {
                 key: fs.readFileSync(`./certs/${process.env.APP_SERVICE}.key`),
                 cert: fs.readFileSync(`./certs/${process.env.APP_SERVICE}.cert`),
             },
+            hmr: {
+                host: `app.${process.env.APP_SERVICE}`,
+                protocol: 'wss',
+                port: 443
+            },
             cors: true,
+            strictPort: true,
         },
         plugins: [
+            basicSsl(),
             laravel({
-                input: 'resources/js/app.js',
+                input: ['resources/js/app.js', 'resources/css/app.css'],
                 refresh: true,
             }),
             tailwindcss(),
